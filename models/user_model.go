1f3316a9b1a59a44e57ea7d00f73a2a5d054d492
DataRohit <rohit.vilas.ingole@gmail.com>
2024-09-13 09:57:33 +0530

    feature: user model added

---
+++ models/user_model.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID           primitive.ObjectID `bson:"_id"`
+	FirstName    *string            `json:"firstName" validate:"required,min=2,max=100"`
+	LastName     *string            `json:"lastName" validate:"required,min=2,max=100"`
+	Password     *string            `json:"password" validate:"required,min=6"`
+	Email        *string            `json:"email" validate:"email,required"`
+	Avatar       *string            `json:"avatar"`
+	Phone        *string            `json:"phone" validate:"required"`
+	AccessToken  *string            `json:"accessToken"`
+	RefreshToken *string            `json:"refreshToken"`
+	CreatedAt    time.Time          `json:"createdAt"`
+	UpdatedAt    time.Time          `json:"updatedAt"`
+	UserID       string             `json:"userId"`
+}
