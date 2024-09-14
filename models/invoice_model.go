891c12a8a7f8451e9fab4080ec82828501abff4a
DataRohit <rohit.vilas.ingole@gmail.com>
2024-09-14 15:26:30 +0530

    feature: invoice model added

---
+++ models/invoice_model.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Invoice struct {
+	ID             primitive.ObjectID `json:"id" bson:"_id"`
+	InvoiceID      string             `json:"invoiceId" bson:"invoiceId"`
+	OrderID        string             `json:"orderId" bson:"orderId"`
+	PaymentMethod  *string            `json:"paymentMethod" bson:"paymentMethod" validate:"eq=CARD|eq=CASH|eq=ONLINE"`
+	PaymentStatus  *string            `json:"paymentStatus" bson:"paymentStatus" validate:"required,eq=PENDING|eq=PAID"`
+	PaymentDueDate time.Time          `json:"paymentDueDate" bson:"paymentDueDate"`
+	CreatedAt      time.Time          `json:"createdAt" bson:"createdAt"`
+	UpdatedAt      time.Time          `json:"updatedAt" bson:"updatedAt"`
+}
